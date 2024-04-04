46613f7c20d8cec3069d70dd98e370e8b7e6673f
assilbekov <askhat.assilbekov@gmail.com>
2024-04-04 05:39:29 -0300

    add card html

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex items-center justify-center w-screen h-screen bg-slate-950 text-white">
+      <div className="card-wrapper h-48 w-48">
+        <div className="card-content flex items-center justify-center text-sm">
+          Beatiful Card
+        </div>
+      </div>
+    </div>
   )
 }
 
